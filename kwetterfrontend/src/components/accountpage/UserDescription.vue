781dfe99eb672eb061861a6a59de9e7c537aee38
Joel Castillo Guerra <joelcastillog@gmail.com>
2019-03-20 17:00:21 +0100

    Profile userdescription made

---
+++ kwetterfrontend/src/components/accountpage/UserDescription.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "UserDescription"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ kwetterfrontend/src/views/AccountPage.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "AccountPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
