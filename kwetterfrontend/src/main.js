d3ce0cd327c9035884aac42e1a6df93e3b5c81eb
Joel Castillo Guerra <joelcastillog@gmail.com>
2019-03-21 21:48:37 +0100

    Added  vuex

--- kwetterfrontend/src/main.js
+++ kwetterfrontend/src/main.js
@@ -3,10 +3,12 @@ import Vue from 'vue'
 // import App from './views/AdminPanel'
 import App from './views/AdminPanel'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
